5726ff6557ce128a4cddc6d81c8c292c27d14783
Alfonso <alf.polanco@gmail.com>
2020-10-11 15:50:20 -0500

    bottom_navigation_view finalizada

--- app/src/main/java/com/axikmobile/androidadmobapp/MainActivity.kt
+++ app/src/main/java/com/axikmobile/androidadmobapp/MainActivity.kt
@@ -2,10 +2,61 @@ package com.axikmobile.androidadmobapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var content: FrameLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        content = findViewById(R.id.fragment_container) as FrameLayout
+        val navigation = findViewById(R.id.bottom_nativation_view) as BottomNavigationView
+        navigation.setOnNavigationItemReselectedListener { myOnNavigationItemSelectedListener }
     }
+
+    private val myOnNavigationItemSelectedListener = object: BottomNavigationView.OnNavigationItemSelectedListener {
+
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            when (item.itemId) {
+
+                R.id.action_banner -> {
+                   // val fragment = BannerFragment()
+                 //   loadFragment(fragment)
+                    return true
+                }
+
+                R.id.action_intersticial -> {
+                   // val fragment = IntersticialFragment()
+                   //loadFragment(fragment)
+                    return true
+
+                }
+
+                R.id.action_video -> {
+                   // val fragment = VideoFragment()
+                   // loadFragment(fragment)
+                    return true
+
+                }
+            }
+              return false
+        }
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.design_bottom_sheet_slide_out,R.anim.design_bottom_sheet_slide_in)
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(fragment.javaClass.simpleName)
+            .commit()
+    }
+
+
 }
